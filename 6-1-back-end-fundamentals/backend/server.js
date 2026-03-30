ebb7ab67c4ab75106fe7dbbb0430992684432ae7
fahadalzubaidi <s202271360@kfupm.edu.sa>
2026-03-30 11:57:00 +0300

    Completed TODO7

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -29,6 +29,12 @@ app.get('/about', (req, res) => {
 
 
 // TODO 7: Create the "/student" route
+app.get('/student', (req, res) => {
+    res.json({
+        name: 'Aisha',
+        major: 'Computer Science'
+    });
+});
 
 
 
